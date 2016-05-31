b406dc7efab0f6daa7508fe2f1c499ab0a983384
wgriffin410 <wgriffin410@bellsouth.net>
2016-05-31 17:27:54 -0400

    Edited R file from the forked repo and added example txt file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions created in this assignment take advantage of R scoping rules to avoid costly
+## repeated computation of inverse matrices by staging to cache the value of the inverse.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        imat <- NULL
+        set <- function(y) {
+                x <<- y
+                imat <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) imat <<- inverse
+        getInverse <- function() imat
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        imat <- x$getInverse()
+        if(!is.null(imat)) {
+              message("getting cached data")
+              return(imat)
+        }
+        mat <- x$get()
+        imat <- solve(mat, ...)
+        x$setInverse(imat)
+        imat
 }
